03e442617eda44d9e23ae13a504ad24fbb0f4fe4
Muhammad Al Mahallawy <mhallawy11@gmail.com>
2026-05-07 17:08:54 +0300

    added sizeof(); to the code

--- sort.c
+++ sort.c
@@ -16,23 +16,23 @@ void swap(int *n1, int *n2){
     *n2 = temp;
 }
 
-
 int main(){
 int Array[] = {12,11,10,9,8};
+int size =(sizeof(Array) / sizeof(Array[0]));
 
-for (int i = 0; i < 5; i++){
+for (int i = 0; i < size; i++){
     printf("%d ", Array[i]);
 }
 printf("\n");
-for (int j = 0; j < 5; j++)
+for (int j = 0; j < size; j++)
 {
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < (size - 1); i++){
         if (compare(Array[i], Array[i+1])){
            swap(&Array[i],&Array[i+1]);
     }
     }
 }
-for (int i = 0; i < 5; i++){
+for (int i = 0; i < size; i++){
     printf("%d ", Array[i]);
 }
 }
