a993b5c50ebbef2bba0249a1203a8ba1a91ad4e8
Muhammad Al Mahallawy <mhallawy11@gmail.com>
2026-05-07 16:08:48 +0300

    added the swap function

--- sort.c
+++ sort.c
@@ -9,13 +9,16 @@ int compare(int x, int y) {
     {
         return 0;
     }
-    
+}
+void swap(int *n1, int *n2){
+    int temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
 }
 
 
 int main(){
 int Array[] = {12,11,10,9,8};
-int Buffer[5];
 
 for (int i = 0; i < 5; i++){
     printf("%d ", Array[i]);
@@ -25,9 +28,7 @@ for (int j = 0; j < 5; j++)
 {
     for (int i = 0; i < 4; i++){
         if (compare(Array[i], Array[i+1])){
-            Buffer[i] = Array[i];
-            Array[i] = Array[i+1];
-            Array[i+1] = Buffer[i];
+           swap(&Array[i],&Array[i+1]);
     }
     }
 }
